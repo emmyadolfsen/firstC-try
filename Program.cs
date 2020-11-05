6ee81746c2917f3dcd31612932a3f2e36d64b272
Emmy Adolfsén <emmy.adolfsen@gmail.com>
2020-11-05 12:07:33 +0100

    Project works with all checks

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using static System.Console;
+﻿using static System.Console; // För att kunna ta bort Console.
 using System;
 
 namespace Moment2
@@ -7,50 +7,94 @@ namespace Moment2
     {
         static void Main(string[] args)
         {
-            Write("År(tex 2020):");                  // Fyll i år
-            double year = double.Parse(ReadLine()); // Gör om input till double för nedre beräkningar
 
-            Write("Månad(1-12):");                  // Fyll i månad i nummer format
-            double m = double.Parse(ReadLine());    // Gör om till integer
-
-            Write("Dag(1-31):");                    // Fyll i dag i nummer format
-            double d = double.Parse(ReadLine());    // Gör om till integer
-
-            if(m < 3)           // Om månad är mindre än 3
-            {          
-            m = m + 12;         // lägg till 12 på månad
-            year = year - 1;    // ta bort 1 från år
+            // ÅR
+            Write("År(tex 2020):");                     // Fyll i år
+            string yearInput = ReadLine();              // Spara input i string
+            int year;                                   // Skapa variabel year
+            if (!int.TryParse(yearInput, out year))     // Kolla om det bara är nummer i input
+            {
+                WriteLine("Fel inmatning. Försök med endast nummer.");  // Annars skriv ut fel inmatning och avsluta
+                Environment.Exit(0);
+            }
+            if (yearInput.Length != 4)                  // Kolla om längden på input är korrekt
+            {
+                WriteLine("Fel format. Försök igen.");  // Annars skriv ut fel format och avsluta
+                Environment.Exit(0);
             }
 
-            double c = Math.Floor(year / 100);     // Plocka ut århundrade genom år/100 och runda av neråt
-            double y = year % 100;                 // Plocka ut året genom år modulus 100
             
+            // MÅNAD
+            Write("Månad(1-12):");                      // Fyll i månad i nummer format
+            string monthInput = ReadLine();             // Spara input i string
+            int m;                                      // Skapa variabel m
+            if (!int.TryParse(monthInput, out m))       // Kolla om det bara är nummer i input
+            {
+                WriteLine("Fel inmatning. Försök med endast nummer.");  // Annars skriv ut fel inmatning och avsluta
+                Environment.Exit(0);
+            }
+            if (m < 1 || m > 12)                        // Kolla om längden på input är korrekt
+            {
+                WriteLine("Fel format för månad. Försök igen.");        // Annars skriv ut fel inmatning och avsluta
+                Environment.Exit(0);
+            }
 
-            WriteLine($"({year})Du är född: {c}{y} - {m} - {d}");  // för att se uträkningen
-            // d m year c y
             
+            // DAG
+            Write("Dag(1-31):");                        // Fyll i dag i nummer format
+            string dayInput = ReadLine();               // Spara input i string
+            int d;                                      // Skapa variabel d
+            if (!int.TryParse(dayInput, out d))         // Kolla om det bara är nummer i input
+            {
+                WriteLine("Fel inmatning. Försök med endast nummer.");  // Annars skriv ut fel inmatning och avsluta
+                Environment.Exit(0);
+            }
+            if (d < 1 || d > 31)                        // Kolla om längden på input är korrekt
+            {
+                WriteLine("Fel format för dag. Försök igen.");  // Annars skriv ut fel inmatning och avsluta
+                Environment.Exit(0);
+            }
 
-            /*
+
+            if(m < 3)                   // Om månad är mindre än 3
+            {          
+            m = m + 12;                 // lägg till 12 på månad
+            year = year - 1;            // ta bort 1 från år
+            }
+
+            int c = (year / 100);       // Plocka ut århundrade genom år/100 och runda av neråt
+            int y = year % 100;         // Plocka ut året genom år modulus 100
             
-            double dayOfWeekCentury = Math.Floor((c/4) + (5*c)) % 7; // Seklets första dag
-            double dayOfWeekYear = (dayOfWeekCentury + c + Math.Floor(c/4) ) % 7;
-            double dayOfWeekMonth = dayOfWeekYear + Math.Floor(((m + 1 ) * 26 ) / 10);
-            double dayOfWeek = dayOfWeekMonth % 7;
-            WriteLine($"c = {dayOfWeekCentury} y = {dayOfWeekYear} m = {dayOfWeekMonth} w = {dayOfWeek}");
-            
-            */
-            double veckodag = (d + Math.Floor((13*(m-1))/5) + y + Math.Floor(y/4) + Math.Floor(c/4) + (5*c) ) % 7;
-            double dag = ((veckodag + 5) % 7) + 1;
+            int day = (d + ((13*(m-1))/5) + y + (y/4) + (c/4) + (5*c) ) % 7;    // Räkna ut veckodag med zellers algoritm
+            int weekDay = ((day + 3) % 7) + 1;                                  // Gör om till ISO standard
         
-            WriteLine($"veckodag innan ändring: {veckodag} och efter: {dag}");
+            WriteLine($"Du är född på en:");
             
-
-            
-
-            // 0 = söndag, 1 = måndag, 2 = tisdag, 3 = onsdag, 4 = torsdag, 5 = fredag, 6 = lördag
-            // 3 = söndag, 4 = måndag, 5 = tisdag, 6 = onsdag, 7 = torsdag, 1 = fredag, 2 = lördag
-            // (bara en uträkning) 4 = söndag, 5 = måndag, 6 = tisdag, 0 = onsdag, 7 = torsdag, 2 = fredag, 3 = lördag
-
+            // switch-sats för att skriva ut rätt dag beroende på resultat
+            switch(weekDay) 
+            {
+                case 1:
+                WriteLine("Måndag");
+                break;
+                case 2:
+                WriteLine("Tisdag");
+                break;
+                case 3: 
+                WriteLine("Onsdag");
+                break;
+                case 4: 
+                WriteLine("Torsdag");
+                break;
+                case 5: 
+                WriteLine("Fredag");
+                break;
+                case 6: 
+                WriteLine("Lördag");
+                break;
+                case 7: 
+                WriteLine("Söndag");
+                break;
+            }
 
         }
     }
